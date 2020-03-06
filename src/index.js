1c35b49464fca4027423d31e34600240f3892088
Kamren1027 <kamren.mccall@Gmail.com>
2020-03-05 22:03:41 -0500

    creating DataTable

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
+import DataTable from './DataTable';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('inputMenu'));
+ReactDOM.render(
+<App/>, document.getElementById('inputMenu'));
+
+ReactDOM.render(<DataTable
+    title="Customers"
+    columns={columns} 
+    data={data}
+    selectableRows // add for checkbox selection
+  />, document.getElementById('dataTable'));
 
 
 serviceWorker.unregister();
