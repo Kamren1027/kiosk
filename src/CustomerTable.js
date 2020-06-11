30317597ded6498abc85533abbf29b4a46341934
Kamren1027 <kamren.mccall@Gmail.com>
2020-06-10 23:36:00 -0400

    load data in table from postgres datasource

--- src/CustomerTable.js
+++ src/CustomerTable.js
@@ -4,31 +4,56 @@ class CustomerTable extends Component {
   constructor(props) {
     super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
     this.state = { //state is by default an object
-        students: [
-          { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
-          { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
-          { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
-          { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
-        ]
+        visits: []
     }
   }
 
+  componentDidMount() {
+    fetch("http://localhost:8080/visit/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            visits: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   renderTableData() {
-    return this.state.students.map((student, index) => {
-      const { id, name, age, email } = student //destructuring
+    return this.state.visits.map((visit, index) => {
+      const { created, email, firstName, id, lastName, phone, section, service, serviceTime, station, tech, waitTime} = visit //destructuring
       return (
           <tr key={id}>
             <td>{id}</td>
-            <td>{name}</td>
-            <td>{age}</td>
+            <td>{firstName}</td>
+            <td>{lastName}</td>
             <td>{email}</td>
+            <td>{created}</td>
+            <td>{phone}</td>
+            <td>{section}</td>
+            <td>{service}</td>
+            <td>{serviceTime}</td>
+            <td>{station}</td>
+            <td>{tech}</td>
+            <td>{waitTime}</td>
           </tr>
       )
     })
   }
 
   renderTableHeader() {
-    let header = Object.keys(this.state.students[0])
+    let header = Object.keys(this.state.visits[0])
     return header.map((key, index) => {
       return <th key={index}>{key.toUpperCase()}</th>
     })
@@ -37,10 +62,10 @@ class CustomerTable extends Component {
   render() {
     return (
       <div>
-          <h1 id='title'>React Dynamic Table</h1>
-          <table id='students'>
+          <h1 id='title'>Customer Queue</h1>
+          <table id='visits'>
             <tbody>
-                <tr>{this.renderTableHeader()}</tr>
+                <tr>{/*this.renderTableHeader()*/}</tr>
                 {this.renderTableData()}
             </tbody>
           </table>
